440fe1820bb8baf40cbd92c016f2fc4cc0e8fdda
Clara <claracruz.alves908@gmail.com>
2025-08-20 19:54:43 -0300

    Pegar um jogo pelo id passado na URL

--- src/main/java/com/clara/dslist/repositories/GameRepository.java
+++ src/main/java/com/clara/dslist/repositories/GameRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //Não precisa ser registrado por extender de um componente já registrado
 //Aqui será feito os comandos em SQL que será utilizado pelo serviço
 public interface GameRepository extends JpaRepository<Games, Long> {
-
 }
--- src/main/java/com/clara/dslist/services/GameService.java
+++ src/main/java/com/clara/dslist/services/GameService.java
@@ -1,12 +1,13 @@
 package com.clara.dslist.services;
 
+import com.clara.dslist.dto.GameDTO;
 import com.clara.dslist.dto.GameMinDTO;
 import com.clara.dslist.entities.Games;
 import com.clara.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 //É registrado como service mas também pode ser registrado como Component
@@ -17,6 +18,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Games result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Games> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(GameMinDTO::new).toList();
