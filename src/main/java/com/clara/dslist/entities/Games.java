bedd3a66fb531e7f532860ee6a4d5e3be30b98e8
Clara <claracruz.alves908@gmail.com>
2025-08-20 10:05:02 -0300

    Game seed

--- src/main/java/com/clara/dslist/entities/Games.java
+++ src/main/java/com/clara/dslist/entities/Games.java
@@ -16,20 +16,24 @@ public class Games {
     @Column(name = "game_year")
     private int year;
     private String genre;
-    private String plataforms;
+    private String platforms;
     private double score;
     private String imgUrl;
+    
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Games(){}
 
-    public Games(long id, String title, int year, String genre, String plataforms, double score, String imgUrl, String shortDescription, String longDescription) {
+    public Games(long id, String title, int year, String genre, String platforms, double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataforms = plataforms;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -52,8 +56,8 @@ public class Games {
         return genre;
     }
 
-    public String getPlataforms() {
-        return plataforms;
+    public String getPlatforms() {
+        return platforms;
     }
 
     public double getScore() {
@@ -88,8 +92,8 @@ public class Games {
         this.genre = genre;
     }
 
-    public void setPlataforms(String plataforms) {
-        this.plataforms = plataforms;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public void setScore(double score) {
