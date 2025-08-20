94f90b681206d87d72305d64aaf2282174848a17
Clara <claracruz.alves908@gmail.com>
2025-08-20 09:44:18 -0300

    Propriedades, entidade e ORM

---
+++ src/main/java/com/clara/dslist/entities/Games.java
@@ -0,0 +1,123 @@
+package com.clara.dslist.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_game")
+public class Games {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private int year;
+    private String genre;
+    private String plataforms;
+    private double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public Games(){}
+
+    public Games(long id, String title, int year, String genre, String plataforms, double score, String imgUrl, String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.plataforms = plataforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPlataforms() {
+        return plataforms;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setPlataforms(String plataforms) {
+        this.plataforms = plataforms;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Games games = (Games) o;
+        return id == games.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
