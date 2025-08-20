f91293467a0080d3f46934241ad93ae753c2bdbf
Clara <claracruz.alves908@gmail.com>
2025-08-20 13:47:26 -0300

    Modelo de domínio implementado

---
+++ src/main/java/com/clara/dslist/entities/Belonging.java
@@ -0,0 +1,50 @@
+package com.clara.dslist.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tab_belonging")
+public class Belonging {
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+    private int position;
+
+    public Belonging() {}
+
+    public Belonging(Games game, GameList list, int position) {
+        id.setGame(game);
+        id.setList(list);
+        this.position = position;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Belonging belonging = (Belonging) o;
+        return Objects.equals(id, belonging.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
